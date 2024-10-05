d27b322f97ace174fae51ba3f892dfd6b1542f5d
Nicolas <njcaceres04@gmail.com>
2024-10-04 20:24:35 -0500

    modified code

--- api/index.js
+++ api/index.js
@@ -1,21 +1,36 @@
 const express = require("express");
-const data = ("./data.json");
+const cors = require("cors");
+
 const app = express();
-const port = 3000;
-app.get("/user-info/:id", async (req, res) => {
-  const id = req.params.id;
-  if (!isNumber(id)) {
-    return res.status(400).json({ error: "Invalid parameter" });
+app.use(cors());
+
+const students = [
+  {
+    id: "1",
+    name: "Jhojan Camilo",
+    lastName: "Jimenez Amaya",
+    email: "jhojanjiam@unisabana.edu.co",
+    universityId: "0000301820"
+  },
+  {
+    id: "2",
+    name: "Nicolas Joel",
+    lastName: "Caceres Parra",
+    email: "nicolascacpa@unisabana.edu.co",
+    universityId: "0000273195"
   }
-  const person = data[Number(id) - 1];
-  if (!person) {
-    return res.status(404).json({ message: "No person found" });
+];
+
+app.get("/user-info/:id?", (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({ error: "ID parameter is required" });
   }
-  res.json(person);
+  const student = students.find(student => student.id === id);
+  if (!student) {
+    return res.status(404).json({ error: "Student not found" });
+  }
+  res.json(student);
 });
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-function isNumber(param) {
-  return !isNaN(Number(param));
-}
+
+module.exports = app;
